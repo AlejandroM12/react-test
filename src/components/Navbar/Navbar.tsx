d7e740a2b53683d906f518b16a3fcd7a86e3bc2a
AlejandroM12 <alejandro.morel1905@gmail.com>
2023-01-06 03:44:09 -0300

    initial commit

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,38 @@
+import { AppBar, IconButton, Toolbar, Typography } from "@mui/material";
+import React from "react";
+import { CustomDialog } from "../CustomDialog";
+import { FavoriteTable } from "./FavoriteTable";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import { dialogOpenSubject$ } from "../CustomDialog/CustomDialog";
+export interface NavbarInterface {}
+
+const Navbar: React.FC<NavbarInterface> = () => {
+  const handleClick = () => {
+    dialogOpenSubject$.setSubject = true;
+    console.log(dialogOpenSubject$);
+  };
+  return (
+    <>
+      <CustomDialog>
+        <FavoriteTable />
+      </CustomDialog>
+      <AppBar position="fixed">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Programming React Test
+          </Typography>
+          <IconButton
+            color="secondary"
+            aria-label="favorites"
+            component="label"
+            onClick={handleClick}
+          >
+            <FavoriteIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
+
+export default Navbar;
